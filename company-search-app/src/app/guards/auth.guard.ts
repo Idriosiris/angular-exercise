5de61abaa9129d42ab579026907cc34fc118679f
Stefan <idriceanu.stefan@gmail.com>
2024-10-22 01:17:39 +0100

    feat(company-search-app): make log-in page nicer and make sure you go straight to company details page after log-in

--- company-search-app/src/app/guards/auth.guard.ts
+++ company-search-app/src/app/guards/auth.guard.ts
@@ -1,15 +1,19 @@
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, CanActivateFn, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard = () => {
-  const authService: AuthService = inject(AuthService);
-  const router: Router = inject(Router);
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const intendedUrl = state.url;
+
+  console.log('AuthGuard - Intended URL:', intendedUrl);
 
   if (authService.isLoggedIn()) {
     return true;
   } else {
-    router.navigate(['/login'], { queryParams: { returnUrl: router.url } });
+    router.navigate(['/login'], { queryParams: { returnUrl: intendedUrl } });
     return false;
   }
 };
